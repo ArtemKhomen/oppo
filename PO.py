6d5785db94827572f6f8db1c2021795b84ed8990
Artem Khomenko <artemkhomen@mail.ru>
2025-10-07 17:00:36 +0700

    Refactor menu functions
    split menu functions into separate functions, remove write_file method

--- PO.py
+++ PO.py
@@ -9,11 +9,6 @@ class TempMeasure:
     def __str__(self):
         return f'{self.date} {self.place} {self.result}'
 
-    def write_file(self):
-        f = open('values.txt', 'a', encoding="utf8")
-        f.write(f'{self.date} {self.place} {self.result}\n')
-        f.close()
-
 def parse_string(text):
     city_pattern = r'"([^"]+)"'
     city = re.findall(city_pattern, text)
@@ -37,6 +32,41 @@ def get_values_arr():
     f.close()
     return measures
 
+def input_measure():
+    f = open('values.txt', 'a', encoding="utf8")
+    new_measure = input('Введите новое измерение: ')
+    date, city, value = parse_string(new_measure)
+    measure = TempMeasure(date, city, value)
+    f.write(f'{measure}\n')
+    f.close()
+
+def print_measures():
+    f = open('values.txt', encoding="utf8")
+    print(f.read())
+    f.close()
+
+def print_measures_range():
+    ran1 = float(input("Введите левую границу диапазона: "))
+    ran2 = float(input("Введите вторую границу диапазона: "))
+    measures = get_values_arr()
+    for measure in measures:
+        if ran1 <= measure.result <= ran2:
+            print(measure)
+
+def sort_measures():
+    measures = get_values_arr()
+    l = len(measures)
+    for i in range(l):
+        for j in range(l - i - 1):
+            if measures[j].result > measures[j + 1].result:
+                temp = measures[j]
+                measures[j] = measures[j + 1]
+                measures[j + 1] = temp
+    f = open('values.txt', 'w', encoding="utf8")
+    for measure in measures:
+        f.write(f'{measure}\n')
+    f.close()
+
 def menu():
     while True:
         print("""
@@ -50,36 +80,13 @@ def menu():
         if choice == 0:
             break
         if choice == 1:
-            f = open('values.txt', 'a', encoding="utf8")
-            new_measure = input('Введите новое измерение: ')
-            date, city, value = parse_string(new_measure)
-            measure = TempMeasure(date, city, value)
-            measure.write_file()
-            f.close()
+            input_measure()
         if choice == 2:
-            f = open('values.txt', encoding="utf8")
-            print(f.read())
-            f.close()
+            print_measures()
         if choice == 3:
-            ran1 = float(input("Введите левую границу диапазона: "))
-            ran2 = float(input("Введите вторую границу диапазона: "))
-            measures = get_values_arr()
-            for measure in measures:
-                if ran1 <= measure.result <= ran2:
-                    print(measure)
+            print_measures_range()
         if choice == 4:
-            measures = get_values_arr()
-            l = len(measures)
-            for i in range(l):
-                for j in range(l-i-1):
-                    if measures[j].result > measures[j+1].result:
-                        temp = measures[j]
-                        measures[j] = measures[j+1]
-                        measures[j+1] = temp
-            f = open('values.txt', 'w')
-            f.close()
-            for measure in measures:
-                measure.write_file()
+            sort_measures()
 
 def main():
     menu()
